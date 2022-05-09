1c7549a148b232ab06e3b017f8d0db45f752d5c8
PeterXKYZ <37281634+PeterXKYZ@users.noreply.github.com>
2022-05-08 21:45:47 -0500

    milestone 3 progess code

--- photo.py
+++ photo.py
@@ -1,3 +1,4 @@
+from errno import EALREADY
 from sys import flags
 import tornado.web
 import tornado.ioloop
@@ -7,6 +8,7 @@ import numpy as np
 
 cl_web = []
 cl_cam = []
+cl_esp = []
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
@@ -15,7 +17,7 @@ class IndexHandler(tornado.web.RequestHandler):
 class WebPageHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         print("webpage WebSocket opened") 
-        cl_web.clear()
+        cl_web.clear() 
         
         # append the active client to global variable <cl_web>
         # so it could be accessed by other handlers
@@ -23,15 +25,14 @@ class WebPageHandler(tornado.websocket.WebSocketHandler):
 
     def on_message(self, message):
         print(message)
-
         # everytime a message arrives at the web client, it
         # passes the message along to the cam client
         for c in cl_cam:
             c.write_message(message)
-
+            
     def on_close(self):
         print("webpage WebSocket closed")
-        cl_web.remove(self)
+        cl_web.remove(self) 
 
     def check_origin(self, origin):
         return True 
@@ -44,6 +45,7 @@ class CamHandler(tornado.websocket.WebSocketHandler):
         cl_cam.append(self)
 
     def on_message(self, message):
+        print(type(message)) 
         for c in cl_web:
             c.write_message(message, True)  # True means to send message as binary
 
@@ -59,10 +61,32 @@ class CamHandler(tornado.websocket.WebSocketHandler):
     def check_origin(self, origin):
         return True  
 
+class EspHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print("esp WebSocket opened")
+        cl_esp.clear()
+        cl_esp.append(self)
+
+    def on_message(self, message):
+        if (message == "button"):
+            for c in cl_web:
+                c.write_message(message) 
+        elif (message == "pir"):
+            for c in cl_cam:
+                c.write_message("photo")        
+
+    def on_close(self):
+        print("esp WebSocket closed")
+        cl_esp.remove(self)
+
+    def check_origin(self, origin):
+        return True  
+
 urls = [
     (r"/", IndexHandler),
     (r"/web", WebPageHandler),
     (r"/cam", CamHandler),
+    (r"/esp", EspHandler),
     # https://stackoverflow.com/questions/32288515/how-to-load-html-image-files-on-the-python-tornado
     (r"/(test.jpg)", tornado.web.StaticFileHandler, {"path": "./"}) # used by html page to load a photo
 ]
